d15ef78c37c81b6fb4877bc26e573bf348817a9b
Emilien Devos <unixfox@users.noreply.github.com>
2017-02-05 22:59:01 +0100

    Oops I forgot to hide the output

--- install.sh
+++ install.sh
@@ -109,7 +109,7 @@ install_vlany_prerequisites ()
         dpkg --add-architecture i386 &>/dev/null
         yes | apt-get update &>/dev/null
         apt-get --yes --force-yes install attr libpam0g-dev libpcap-dev libssl-dev gcc-multilib build-essential &>/dev/null
-        apt-get --yes --force-yes install libssl-dev:i386
+        apt-get --yes --force-yes install libssl-dev:i386 &>/dev/null
         [ ! -z "$(apt-cache search libpcap0.8-dev)" ] && { apt-get --yes --force-yes install libpcap0.8-dev &>/dev/null; }
         grep -i ubuntu /proc/version &>/dev/null && rm -f /etc/init/plymouth* &>/dev/null
     elif [ -f /usr/bin/pacman ]; then
